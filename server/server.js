11000f03255ec89eaf311870c0e0838396cc119a
unknown <shawnpark2397@gmail.com>
2023-09-13 06:55:13 -0700

    heroku

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-
-const PORT = process.env.PORT || 3001;
+const { authMiddleware } = require('./utils/auth');
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -21,20 +21,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 db.once('open', () => {
-  startApolloServer();
+  app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
+  console.log(
+    `GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`
+  );
 });
-
-const startApolloServer = async () => {
-  await server.start();
-  server.applyMiddleware({ app });
-
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-};
