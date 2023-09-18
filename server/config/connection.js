db77fbed774285730474b2e42044edae3eb05afb
unknown <shawnpark2397@gmail.com>
2023-09-18 15:40:51 -0700

    h

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mydatabase', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: false,
-  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
